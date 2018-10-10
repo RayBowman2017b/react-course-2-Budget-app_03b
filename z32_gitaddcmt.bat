f4a31a58379d48c71eb030fd25a392010f07c78c
RayBowman2017b <RayBowman2017b@gmail.com>
2018-10-09 23:38:59 -0500

    2018-10-09 23-33 Last Test Failed

--- z32_gitaddcmt.bat
+++ z32_gitaddcmt.bat
@@ -7,7 +7,7 @@ git commit -m "2018-10-09 23-33 Last Test Failed"
 @rem 
 @rem git push origin master
 @rem git push Budget_app master
- git push Budget_app_03 master
+ git push Budget_app_03b master
 @rem 
 @rem git push heroku master
 pause
