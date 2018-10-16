d45fbfefa0c7a5b826d472652c91fc898def6c77
RayBowman2017b <RayBowman2017b@gmail.com>
2018-10-16 16:38:05 -0500

    2018-10-16 16-40 Error Handling Corrections v2

--- src/sec009a_components/sec009a_SFC_expense_dashboard_page.jsx
+++ src/sec009a_components/sec009a_SFC_expense_dashboard_page.jsx
@@ -13,6 +13,8 @@ import SFC_expense_list from "./sec011a_L101_expense_list.jsx";
 //import CLS_expense_list_filters from "./sec011a_L103_expense_list_filters.jsx";
 import ExpenseListFilters from "./sec011a_L103_expense_list_filters.jsx";
 
+import SFC_expenses_summary from './sec013a_L140_expenses_summary.jsx';
+
 import CLS_ErrorBoundary from "../Utilities_01/Error_Boundaries.jsx";
 
     //<CLS_ErrorBoundary><CLS_expense_list_filters /></CLS_ErrorBoundary>
@@ -20,7 +22,8 @@ import CLS_ErrorBoundary from "../Utilities_01/Error_Boundaries.jsx";
 const GC_expense_dashboard_page = () =>
 (
   <div>
-    <div>This is from my dashboard component.</div>
+    <span style={{padding: 1 + "rem"}}>This is from my dashboard component.</span>
+    <SFC_expenses_summary />
     <CLS_ErrorBoundary><ExpenseListFilters /></CLS_ErrorBoundary>
     <SFC_expense_list />
   </div>
